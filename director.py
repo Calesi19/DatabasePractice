b24bd224f64c22f67998fa6c2dc1b5d090f888de
Carlos Lespin <carlos.lespin.silva@gmail.com>
2022-06-25 01:19:21 -0600

    added dynamodb

--- director.py
+++ director.py
@@ -4,12 +4,14 @@ class Director:
     def __init__(self):
         import mongodb
         import firestore
+        import dynamodb
         import os
         import time
         self.clear = lambda: os.system('cls')
         self.__firestoreDB = firestore.Firestore()
         self.__mongoDB = mongodb.MongoDatabase()
-        self.__databases = [self.__firestoreDB, self.__mongoDB] 
+        self.__dynamoDB = dynamodb.DynamoDB()
+        self.__databases = [self.__firestoreDB, self.__mongoDB, self.__dynamoDB] 
 
 
     def main(self):
@@ -17,15 +19,16 @@ class Director:
         while True:
             choice = None
             print(f"\nSelect one of the following options:\n1.Create New Entry\n2.Read all entries\n3.Update Entry\n4.Delete Entry\n5.Delete All Entries")
-            while choice not in range(1, 6):
-                choice = int(input("\nEnter a number choice >"))
+            while choice not in ["1", "2", "3", "4", "5"]:
+                choice = (input("\nEnter a number choice >"))
 
+            choice = int(choice)
             dbchoice = None
-            print(f"\nWhich database would you wish to choose:\n1.Firestore\n2.MongoDB")
-            while dbchoice not in range(1, 3):
-                dbchoice = int(input("\nEnter a number choice >")) - 1
+            print(f"\nWhich database would you wish to choose:\n1.Firestore\n2.MongoDB\n3.DynamoDB")
+            while dbchoice not in ["1", "2", "3"]:
+                dbchoice = input("\nEnter a number choice >")
 
-            database = self.__databases[dbchoice]
+            database = self.__databases[int(dbchoice) - 1]
 
             if choice == 1:
                 self.clear()
---
+++ dynamodb.py
@@ -0,0 +1,88 @@
+
+class DynamoDB:
+    def __init__(self):
+        import boto3
+        import requests
+        import constants
+        self.__client = boto3.client('dynamodb',aws_access_key_id='AKIAZRXTX5EAJAGAFLNI', aws_secret_access_key='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', region_name='us-west-1')
+        self.__resource = boto3.resource('dynamodb',aws_access_key_id='AKIAZRXTX5EAJAGAFLNI', aws_secret_access_key='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', region_name='us-west-1')
+        self.__table = self.__resource.Table('playerScores')
+        self.__name = "DynamoDB"
+
+        
+    def getDatabaseName(self):
+        return self.__name
+
+    def createEntry(self, userName, score):
+        item = { "username": {"S": userName},"score":{"N": f"{score}"}}
+        self.__client.put_item(TableName="playerScores", Item=item)
+    
+    def readEntries(self):
+        paginator = self.__client.get_paginator("scan")
+        for page in paginator.paginate(TableName='playerScores'):
+            for item in page["Items"]:
+                score = int(item['score']['N'])
+                userName = item['username']['S']
+                print(f'\nUsername: {userName}\nScore: {score}')
+
+    def readEntry(self, userName):
+        response = self.__table.get_item(Key={'username': userName})
+        print(f"\nUsername: {response['Item']['username']}\nScore: {response['Item']['score']}")
+
+
+    def updateEntry(self, userName, score):
+        item = { "username": {"S": userName},"score":{"N": f"{score}"}}
+        self.__client.put_item(TableName="playerScores", Item=item)
+    
+    def deleteEntry(self, userName):
+        response = table.delete_item(Key = {'username': userName})
+
+    def deleteAll(self):
+        tableKeyNames = [key.get("AttributeName") for key in self.__table.key_schema]
+
+        #Only retrieve the keys for each item in the table (minimize data transfer)
+        projectionExpression = ", ".join('#' + key for key in tableKeyNames)
+        expressionAttrNames = {'#'+key: key for key in tableKeyNames}
+        
+        counter = 0
+        page = self.__table.scan(ProjectionExpression=projectionExpression, ExpressionAttributeNames=expressionAttrNames)
+        with self.__table.batch_writer() as batch:
+            while page["Count"] > 0:
+                counter += page["Count"]
+                
+                for itemKeys in page["Items"]:
+                    batch.delete_item(Key=itemKeys)
+                
+                if 'LastEvaluatedKey' in page:
+                    page = self.__table.scan(
+                        ProjectionExpression=projectionExpression, ExpressionAttributeNames=expressionAttrNames,
+                        ExclusiveStartKey=page['LastEvaluatedKey'])
+                else:
+                    break
+
+
+    def getEntries(self):
+        entries = {}
+        paginator = self.__client.get_paginator("scan")
+        for page in paginator.paginate(TableName='playerScores'):
+            for item in page["Items"]:
+                score = int(item['score']['N'])
+                userName = item['username']['S']
+                entries[userName] = score
+        return entries
+
+    def populate(entries):
+        for value in entries:
+            self.createEntry(value, entries[values])
+        
+
+'''
+db = DynamoDB()
+db.createEntry("test1", 400)
+db.createEntry("test4", 2)
+#db.updateEntry("Carlos", 5)
+db.readEntries()
+
+dic = db.getEntries()
+print(dic)
+'''
